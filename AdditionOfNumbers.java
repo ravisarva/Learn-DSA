392d217314c5c462dca48124377cddd9848792ec
ravisarva <60805477+ravisarva@users.noreply.github.com>
2023-09-26 17:12:32 +0530

    addtion of two number

---
+++ AdditionOfNumbers.java
@@ -0,0 +1,6 @@
+//Url: https://practice.geeksforgeeks.org/problems/addition-of-two-numbers0812/1
+public class AdditionOfNumbers {
+    static int addition(int A, int B){
+        return A + B;
+    }
+}
