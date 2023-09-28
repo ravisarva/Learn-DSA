c7ccfd0920dfef212f47665ed1bd3779a9966e45
ravisarva <60805477+ravisarva@users.noreply.github.com>
2023-09-28 20:52:11 +0530

    added the solution of remove duplicate from a sorted array method 1.

---
+++ week2/RemoveDuplicate.java
@@ -0,0 +1,15 @@
+class RemoveDuplicate {
+    public int removeDuplicates(int[] nums) {
+        int count=0;
+        ArrayList<Integer> lst=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(!lst.contains(nums[i])){
+                lst.add(nums[i]);
+            }
+        }
+        for(int i=0;i<lst.size();i++){
+            nums[i]=lst.get(i);
+        }
+        return lst.size();
+    }
+}
