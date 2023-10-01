a2ac18e404531409d2d830e779eb4f55f4c015de
ravisarva <60805477+ravisarva@users.noreply.github.com>
2023-10-01 15:08:25 +0530

    added the solution of two sum using hash map.

---
+++ week2/TwoSum.java
@@ -0,0 +1,16 @@
+URL: https://leetcode.com/problems/two-sum/description/
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< n; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[] {map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
