4787832c63f7146ebb320ecb406405ddfe864e30
ravisarva <60805477+ravisarva@users.noreply.github.com>
2023-10-07 21:41:10 +0530

    added the solution of sort color problem.

---
+++ week4/SortColor.java
@@ -0,0 +1,28 @@
+class SortColor {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else {
+                int temp1 = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp1;
+                high--;
+            }
+        }
+
+    }
+}
